f93294551ecb388bf888b46ffded7a464df51e3a
Vaibhav Godase <123892582+VaibhavGodase@users.noreply.github.com>
2025-03-06 21:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+document.getElementById('button').addEventListener('click',function () {
+	  let name = document.getElementById("name").value;
+            let year = document.getElementById("year").value;
+            let baseUrl = "https://localhost:8080/";
+
+
+	 
+            if (name || year) {
+                let params = new URLSearchParams();
+                if (name) params.append("name", name);
+                if (year) params.append("year", year);
+                
+                document.getElementById("url").textContent = baseUrl + "?" + params.toString();
+            } else {
+                document.getElementById("url").textContent = baseUrl;
+            }
+})
